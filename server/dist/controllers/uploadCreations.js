74214d2ed80994d8eede0f85c704a69b3a2c1e30
Kylian Dupouy Peron <kdperon@yahoo.com>
2023-04-02 02:38:17 +0200

    commit

--- server/dist/controllers/uploadCreations.js
+++ server/dist/controllers/uploadCreations.js
@@ -22,102 +22,97 @@ function uploadCreations(req, res) {
         if (!Number.isNaN(req.headers["content-length"])) {
             if (req.files && typeof req.files.length == "number" && req.files.length >= 1) {
                 if (Number(req.headers["content-length"]) <= 4000000 * req.files.length) {
-                    if (typeof req.session.adminConnected === "boolean" && req.session.adminConnected === true) {
-                        var readyToUpload = true;
-                        var filesPath = [];
-                        const files = req.files;
-                        files.forEach((file) => __awaiter(this, void 0, void 0, function* () {
-                            const { buffer } = file;
-                            var picturePath;
-                            var checkIfPathExist = false;
-                            var count = 0;
-                            do {
-                                count++;
-                                var picturePath = crypto_1.default.randomUUID() + crypto_1.default.randomUUID() + crypto_1.default.randomUUID() + crypto_1.default.randomUUID();
-                                checkIfPathExist = fs_1.default.existsSync(path_1.default.join(__dirname, "../../creations/" + picturePath));
-                            } while (checkIfPathExist && count <= 10);
-                            if (count >= 12) {
-                                readyToUpload = false;
-                            }
-                            else {
-                                filesPath.push(picturePath);
-                            }
-                        }));
-                        if (readyToUpload) {
-                            var filesSuccess = []; // [resizedSuccess, realSizeSuccess]
-                            for (let i = 0; i < files.length; i++) {
-                                const file = files[i];
-                                if (filesPath[i] !== undefined && typeof filesPath[i] === "string") {
-                                    const { buffer } = file;
-                                    var image = (0, sharp_1.default)(buffer);
-                                    const metadata = yield image.metadata();
-                                    let rotate = 0;
-                                    if (metadata.orientation === 6) {
-                                        rotate = 90;
-                                    }
-                                    else if (metadata.orientation === 8) {
-                                        rotate = 270;
-                                    }
-                                    else if (metadata.orientation === 3) {
-                                        rotate = 180;
-                                    }
-                                    yield new Promise((resolve) => {
-                                        image = image.rotate(rotate);
-                                        image.webp({ quality: 60 }).toFile(path_1.default.join(__dirname, "../../creations/resized/" + filesPath[i] + ".webp"), (err, info) => {
-                                            if (err) {
-                                                (0, globalFunc_1.log)(err, true);
-                                                filesSuccess.push([false, false]);
-                                                resolve();
-                                            }
-                                            else {
-                                                filesSuccess.push([true]);
-                                                image.webp().toFile(path_1.default.join(__dirname, "../../creations/" + filesPath[i] + ".webp"), (err, info) => {
-                                                    resolve();
-                                                    if (err) {
-                                                        (0, globalFunc_1.log)(err, true);
-                                                        filesSuccess[i].push(false);
-                                                    }
-                                                    else {
-                                                        filesSuccess[i].push(true);
-                                                    }
-                                                });
-                                            }
-                                        });
-                                    });
-                                }
-                                else {
-                                    filesSuccess.push([false, false]);
-                                }
-                            }
-                            var filesError = [];
-                            for (let i = 0; i < filesSuccess.length; i++) {
-                                const fileSuccess = filesSuccess[i];
-                                if (!fileSuccess[0] || !fileSuccess[1]) {
-                                    filesError.push(files[i].filename);
-                                }
-                                if (!fileSuccess[0] && fileSuccess[1]) {
-                                    fs_1.default.unlink(path_1.default.join(__dirname, "../../creations/" + filesPath[i] + ".webp"), () => { });
-                                }
-                                else if (fileSuccess[0] && !fileSuccess[1]) {
-                                    fs_1.default.unlink(path_1.default.join(__dirname, "../../creations/resized/" + filesPath[i] + ".webp"), () => { });
-                                }
-                            }
-                            if (filesError.length == 0) {
-                                for (let i = 0; i < filesPath.length; i++) {
-                                    filesPath[i] = filesPath[i] + ".webp";
-                                }
-                                res.status(200).json(filesPath);
-                            }
-                            else {
-                                res.status(500).json({ "message": "Une erreur interne est survenue", "infos": filesError });
-                            }
+                    var readyToUpload = true;
+                    var filesPath = [];
+                    const files = req.files;
+                    files.forEach((file) => __awaiter(this, void 0, void 0, function* () {
+                        const { buffer } = file;
+                        var picturePath;
+                        var checkIfPathExist = false;
+                        var count = 0;
+                        do {
+                            count++;
+                            var picturePath = crypto_1.default.randomUUID() + crypto_1.default.randomUUID() + crypto_1.default.randomUUID() + crypto_1.default.randomUUID();
+                            checkIfPathExist = fs_1.default.existsSync(path_1.default.join(__dirname, "../../creations/" + picturePath));
+                        } while (checkIfPathExist && count <= 10);
+                        if (count >= 12) {
+                            readyToUpload = false;
                         }
                         else {
-                            res.status(500).json({ "message": "Une erreur interne est survenue, réessayez plus tard" });
+                            filesPath.push(picturePath);
+                        }
+                    }));
+                    if (readyToUpload) {
+                        var filesSuccess = []; // [resizedSuccess, realSizeSuccess]
+                        for (let i = 0; i < files.length; i++) {
+                            const file = files[i];
+                            if (filesPath[i] !== undefined && typeof filesPath[i] === "string") {
+                                const { buffer } = file;
+                                var image = (0, sharp_1.default)(buffer);
+                                const metadata = yield image.metadata();
+                                let rotate = 0;
+                                if (metadata.orientation === 6) {
+                                    rotate = 90;
+                                }
+                                else if (metadata.orientation === 8) {
+                                    rotate = 270;
+                                }
+                                else if (metadata.orientation === 3) {
+                                    rotate = 180;
+                                }
+                                yield new Promise((resolve) => {
+                                    image = image.rotate(rotate);
+                                    image.webp({ quality: 60 }).toFile(path_1.default.join(__dirname, "../../creations/resized/" + filesPath[i] + ".webp"), (err, info) => {
+                                        if (err) {
+                                            (0, globalFunc_1.log)(err, true);
+                                            filesSuccess.push([false, false]);
+                                            resolve();
+                                        }
+                                        else {
+                                            filesSuccess.push([true]);
+                                            image.webp().toFile(path_1.default.join(__dirname, "../../creations/" + filesPath[i] + ".webp"), (err, info) => {
+                                                resolve();
+                                                if (err) {
+                                                    (0, globalFunc_1.log)(err, true);
+                                                    filesSuccess[i].push(false);
+                                                }
+                                                else {
+                                                    filesSuccess[i].push(true);
+                                                }
+                                            });
+                                        }
+                                    });
+                                });
+                            }
+                            else {
+                                filesSuccess.push([false, false]);
+                            }
+                        }
+                        var filesError = [];
+                        for (let i = 0; i < filesSuccess.length; i++) {
+                            const fileSuccess = filesSuccess[i];
+                            if (!fileSuccess[0] || !fileSuccess[1]) {
+                                filesError.push(files[i].filename);
+                            }
+                            if (!fileSuccess[0] && fileSuccess[1]) {
+                                fs_1.default.unlink(path_1.default.join(__dirname, "../../creations/" + filesPath[i] + ".webp"), () => { });
+                            }
+                            else if (fileSuccess[0] && !fileSuccess[1]) {
+                                fs_1.default.unlink(path_1.default.join(__dirname, "../../creations/resized/" + filesPath[i] + ".webp"), () => { });
+                            }
+                        }
+                        if (filesError.length == 0) {
+                            for (let i = 0; i < filesPath.length; i++) {
+                                filesPath[i] = filesPath[i] + ".webp";
+                            }
+                            res.status(200).json(filesPath);
+                        }
+                        else {
+                            res.status(500).json({ "message": "Une erreur interne est survenue", "infos": filesError });
                         }
                     }
                     else {
-                        res.status(401).json({ "message": "Vous n'êtes pas connecté" });
+                        res.status(500).json({ "message": "Une erreur interne est survenue, réessayez plus tard" });
                     }
                 }
                 else {
